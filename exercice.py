ae61775e19cfd9a357cd1401413dbcdeab5e33e6
JerPatterson <you@example.com>
2021-10-25 10:40:17 -0400

    ...

--- exercice.py
+++ exercice.py
@@ -39,6 +39,116 @@ def draw_branch(branch_len, pen_size, angle):
         turtle.right(angle)
         turtle.backward(branch_len)
 
+"""def draw_tree(branch_len = 70, pen_size = 7, angle = 35): 
+    turtle.pencolor("green")
+    turtle.left(90)
+    turtle.pensize(pen_size)
+    turtle.forward(branch_len)
+    branch_len -= 10
+    pen_size -= 1
+
+    def left(angle, branch, branch_distance):
+        if branch > 0 and branch_distance > 0: 
+            turtle.pendown()
+            turtle.pensize(branch)
+            turtle.left(angle)
+            turtle.forward(branch_distance)
+            left(angle-5, branch-1, branch_distance-10)
+
+    def right(angle, branch, branch_distance):
+        if branch > 0 and branch_distance > 0: 
+            turtle.pendown()
+            turtle.pensize(branch)
+            turtle.right(angle)
+            turtle.forward(branch_distance)
+            right(angle-5, branch-1, branch_distance-10)
+
+    def back_left(angle, branch, branch_distance):
+        turtle.penup()
+        if branch > 0:
+            turtle.backward(branch_distance)
+            turtle.right(angle)
+            back_left(angle+5, branch-1, branch_distance+10)
+            
+
+    def back_right(angle, branch, branch_distance):
+        turtle.penup()
+        if branch > 0:
+            turtle.backward(branch_distance)
+            turtle.left(angle)
+            back_right(angle+5, branch-1, branch_distance+10)
+
+    def other_side_left(angle_min, level, len_min):
+        for number in range(1, level):
+            left(angle_min+5*(number-1), number, len_min+10*(number-1))
+            if number == 1:
+                back_left(angle_min, 1, len_min)
+                back_right(angle_min+5*number, 1, len_min+10*number)
+            elif number > 1:
+                back_left(angle_min, 1, len_min)
+                for occurence in range(1, number):
+                    right(angle_min+5*(occurence-1), occurence, len_min+10*(occurence-1))
+                    back_right(angle_min, occurence, len_min)
+                    back_left(angle_min+5*occurence, 1, len_min+10*occurence)
+                back_right(angle_min+5*number, 1, len_min+10*number)
+
+    def other_side_right(angle_min, level, len_min):
+        for number in range(1, level):
+            right(angle_min+5*(number-1), number, len_min+10*(number-1))
+            if number == 1:
+                back_right(angle_min, 1, len_min)
+                back_left(angle_min+5*number, 1, len_min+10*number)
+            elif number > 1:
+                back_right(angle_min, 1, len_min)
+                for occurence in range(1, number):
+                    left(angle_min+5*(occurence-1), occurence, len_min+10*(occurence-1))
+                    back_left(angle_min, occurence, len_min)
+                    back_right(angle_min+5*occurence, 1, len_min+10*occurence)
+                back_left(angle_min+5*number, 1, len_min+10*number)
+
+            
+    def side_left(angle, pen_size, branch_len):
+        left(angle, pen_size, branch_len)
+        angle_left = angle_min = angle - (pen_size-1)*5
+        len_left = len_min = branch_len - (pen_size-1)*10
+        back_left(angle_min, 1, len_min)
+        for level in range(1, pen_size):
+            right(angle_left, level, len_left)
+            if level == 1:
+                back_right(angle_min, level, len_min)
+                angle_left += 5
+                len_left += 10
+                back_left(angle_left, 1, len_left)
+            elif level > 1:
+                back_right(angle_min, 1, len_min)
+                other_side_left(angle_min, level, len_min)
+                angle_left += 5
+                len_left += 10
+                back_left(angle_left, 1, len_left)
+
+    def side_right(angle, pen_size, branch_len):
+        right(angle, pen_size, branch_len)
+        angle_right = angle_min = angle - (pen_size-1)*5
+        len_right = len_min = branch_len - (pen_size-1)*10
+        back_right(angle_min, 1, len_min)
+        for level in range(1, pen_size):
+            left(angle_right, level, len_right)
+            if level == 1:
+                back_left(angle_min, level, len_min)
+                angle_right += 5
+                len_right += 10
+                back_right(angle_right, 1, len_right)
+            elif level > 1:
+                back_left(angle_min, 1, len_min)
+                other_side_right(angle_min, level, len_min)
+                angle_right += 5
+                len_right += 10
+                back_right(angle_right, 1, len_right)
+    
+    
+    side_left(angle, pen_size, branch_len)
+    side_right(angle, pen_size, branch_len)"""
+
 
 def validation_adn(adn_chain):
     if len(adn_chain) != 0:
